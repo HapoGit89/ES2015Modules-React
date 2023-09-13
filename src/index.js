1433f99598a5e6402eb25d0f147463f032a59d91
Hannes Porombka <hannes.porombka@gmail.com>
2023-09-13 18:16:53 +0200

    added to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import {shuffle, remove} from "./helpers"
+import fruits from "./foods"
+
+const randomFruit = shuffle(fruits)[0]
 
 
-console.log(shuffle([2,6,4,5,7,2]))
-console.log(remove(["red", "yellow", "green"], "red"))
+console.log( `Hello I'd like to have one ${randomFruit}`)
+console.log(`Heres your ${randomFruit}`)
+console.log("Thanks, could I have another?")
+console.log(`Sorry we are now out of ${randomFruit}`)
+console.log(`We have ${remove(fruits,randomFruit)} left.`)
